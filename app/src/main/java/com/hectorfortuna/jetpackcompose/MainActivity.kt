5cb1f974fbec945a8537c69426a09695b2c349c4
Hector Fortuna <hectorfortuna@brq.com>
2022-09-12 13:42:21 -0300

    Initial commit

---
+++ app/src/main/java/com/hectorfortuna/jetpackcompose/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.hectorfortuna.jetpackcompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.hectorfortuna.jetpackcompose.ui.theme.JetpackComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            JetpackComposeTheme {
+               Greeting(name = "Hector")
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+        Greeting("Android")
+}
